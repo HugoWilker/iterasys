9d126aed5ba7e850014ebf711e3e1574e56e9978
Hugo <hugowilker@gmail.com>
2021-06-10 04:25:02 -0300

    criação de metodos contagem regressiva, criação da classe scanner.

--- Intruducao-java/src/main/java/introducao/Medidas.java
+++ Intruducao-java/src/main/java/introducao/Medidas.java
@@ -1,40 +1,105 @@
+
+
 // 1 do Pacote
 package introducao;
 
 // 2 Referenciar as bibliotecas
 
 
+import java.util.Scanner;
+
 // 3 clsses
 public class Medidas {
 
-     // atributos - caracteristicas
+    // atributos - caracteristicas
 
     // método ou função
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
         // calculo de area  - exemplo: o tamanho do tapete ou piso.
-     calcularAreaModoCompacto();
-     calcularAreaModoExtenso();
+
+        // utilizar a classe scanner para ler a escolha do usuário no console.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("O P Ç O Ê  S");
+
+        System.out.println("C - Calcular área Modo Curto ");
+        System.out.println("E - Calcular àrea Modo Extenso ");
+        System.out.println("I - If simples ");
+        System.out.println("D - Contar de 1 até 10");
+        System.out.println("R - Contagem Regressiva");
+        String opcao = scanner.next();
+
+        //Condicional (IF = se). (switch = seleciona o comportamento do programa conforme a escolha do pessoa ou software )
+
+        //String opcao = "melancia";
+
+        switch (opcao) {
+
+            case "i":
+            case "I":
+                System.out.println("você escolheu o método ifSimples");
+                ifSimples();
+                break;
+            case "C":
+            case "c":
+                System.out.println("você escolheu o método compacto");
+                calcularAreaModoCompacto();
+                break;
+            case "E":
+            case "e":
+                System.out.println("você escolheu o método extenso");
+                calcularAreaModoExtenso();
+                break;
+            case "D":
+            case "d":
+                System.out.println("você escolheu contar ate dez");
+                contarAteDez();
+                break;
+            case "R":
+            case "r":
+                System.out.println("você escolheu contagem regressiva");
+                contagemRegressiva();
+                break;
+            default:
+                System.out.println("escolha uma opção válida");
 
 
+        }
 
 
     }
 
-    public static void calcularAreaModoExtenso(){
 
-        System.out.println( "Calculo e áreas modo extenso!!");
+    public static void ifSimples() {
 
-    int largura;
-    int comprimento;
-    int resultado;
+        String modo = "extenso";
 
-    largura = 5;
-    comprimento = 3;
+        if (modo == "compacto") {
 
-    resultado = largura * comprimento;
+            calcularAreaModoCompacto();
+        } else {
+            calcularAreaModoExtenso();
 
+        }
+
+
+    }
+
+    public static void calcularAreaModoExtenso() {
+
+        System.out.println("Calculo e áreas modo extenso!!");
+
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5;
+        comprimento = 3;
+
+        resultado = largura * comprimento;
 
 
         System.out.println("Para largura de " + largura + " e comprimento de " + comprimento + " a área é de " + resultado + "M2");
@@ -43,17 +108,41 @@ public class Medidas {
     }
 
 
-public static void calcularAreaModoCompacto(){
+    public static void calcularAreaModoCompacto() {
 
-    System.out.println( "Calculo e áreas modo compacto!!");
+        System.out.println("Calculo e áreas modo compacto!!");
 
         int largura = 4;
         int comprimento = 3;
 
 
+        System.out.println("para a largura de " + largura + " para o comprimento de " + comprimento + " resulta é: " + largura * comprimento + "M2");
+        System.out.println("área de " + largura * comprimento);
+    }
 
-    System.out.println("para a largura de " + largura + " para o comprimento de " + comprimento + " resulta é: " + largura * comprimento + "M2"  );
-   System.out.println("área de " + largura*comprimento);
+    public static void contarAteDez() {
+//loop ou estrutura de repetição.
+// for - repetição incondicional
+
+        System.out.println("Contagem crescente");
+
+        for (int numero = 1; numero <= 10; numero++) {
+
+            System.out.println(numero);
+
+        }
+    }
+
+
+    public static void contagemRegressiva() {
+        System.out.println("Contagem Regressiva");
+
+        for (int numero = 10; numero > -1; numero--) {
+
+            System.out.println(numero);
+
+        }
+    }
 }
 
-}
+
